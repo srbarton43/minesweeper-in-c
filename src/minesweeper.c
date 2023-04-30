6cc189ead94a77b97a9a9b0c020cd1a20150f0a3
Samuel R. Barton <f005cz5@babylon1.thayer.dartmouth.edu>
2023-04-30 15:23:17 -0400

    Create and print empty board to stdout

--- src/minesweeper.c
+++ src/minesweeper.c
@@ -6,8 +6,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct board {
+  int r,c;
+  int minesLeft;
+  int** brd;
+} board_t;
+
+static board_t* createBoard(int r, int c, int numMines);
+static void printBoard(board_t* board);
+
 int
 main (int argc, char** argv) {
-  printf("hello world\n");
+  board_t* board = createBoard(4, 5, 2);
+  printBoard(board);
   return 0;
+}
+
+static board_t*
+createBoard (int r, int c, int numMines) {
+  board_t* b= malloc(sizeof(board_t));
+  b->r = r;
+  b->c = c;
+  b->minesLeft = numMines;
+  b->brd = malloc(r*sizeof(int));
+  for (int i=0; i<r; i++) {
+    b->brd[i] = malloc(c*sizeof(int));
+  }
+  return b;
+}
+
+static void
+printBoard (board_t* board) {
+  for(int r = 0; r < board->r; r++) {
+    for(int c = 0; c < board->c; c++) {
+      printf("[] ");
+    }
+    printf("\n");
+  }
 }
